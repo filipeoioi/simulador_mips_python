bf4514b7f627a158bb2b6caed7769103aa8915c3
Filipe Augusto <filipeaugusto@alunos.utfpr.edu.br>
2022-11-15 12:16:27 -0300

    Finalizada a função sub e soma

--- checkpoin_01/sum_sub.py
+++ checkpoin_01/sum_sub.py
@@ -28,13 +28,26 @@ def somador_basico(num1, num2):
         elif num1[count] == '0' and num2[count] == '0' and sobra == '1':
             res.append('1')
             sobra = '0'
-        print(res)
     res = ''.join(res)
     res = res[::-1]
     return res
 
-num1 = '0001'
-num2 = '0010'
-#Resultado = '0100'
-res = somador_basico(num1, num2)
-print(res)
+def complemento_2(num):
+    res = []
+    numero1 = '0001'
+    for count in range(0, len(num)):
+        if num[count] == '1':
+            res.append('0') 
+        elif num[count] == '0':
+            res.append('1')
+    res = ''.join(res)
+    res = somador_basico(res, numero1)
+    num = res
+    return num
+
+def sub(num1, num2):
+    num2 = complemento_2(num2)
+    res = somador_basico(num1, num2)
+    return res
+
+
---
+++ checkpoin_01/teste_import.py
@@ -0,0 +1,8 @@
+from sum_sub import somador_basico, sub
+num1 = '0001'
+num2 = '0001'
+
+res = somador_basico(num1, num2)
+print('Resultado da soma: ', res)
+res = sub(num1, num2)
+print('Resultado da subtração: ', res)
